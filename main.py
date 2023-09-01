cd06db26febc4cf1bd6c4b9785da6b8b60fead31
Євген <astartessoritas@gmail.com>
2023-09-01 20:16:54 +0300

    Script_2

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import shutil
 import sys
 import unicodedata
 from pathlib import Path
+import zipfile
+import tarfile
+import gzip
 
 IMAGE_EXTENSIONS = ('JPEG', 'PNG', 'JPG', 'SVG')
 VIDEO_EXTENSIONS = ('AVI', 'MP4', 'MOV', 'MKV')
@@ -16,13 +19,27 @@ def normalize(text):
     normalized_text = ''.join(c if c in allowed_chars else '_' for c in normalized_text)
     return normalized_text
 
-
-
+def extract_archive(archive_path, destination_folder):
+    archive_path = Path(archive_path)
+    destination_folder = Path(destination_folder)
+    
+    if archive_path.suffix == '.zip':
+        with zipfile.ZipFile(archive_path, 'r') as zip_ref:
+            zip_ref.extractall(destination_folder)
+    elif archive_path.suffix == '.tar':
+        with tarfile.open(archive_path, 'r') as tar_ref:
+            tar_ref.extractall(destination_folder)
+    elif archive_path.suffix == '.gz':
+        with gzip.open(archive_path, 'rb') as gz_ref:
+            with tarfile.open(fileobj=gz_ref, mode='r') as tar_ref:
+                tar_ref.extractall(destination_folder)
+    else:
+        print(f"Unsupported archive format: {archive_path.suffix}")
 
 def process_folder(folder_path):
     for item in os.listdir(folder_path):
         item_path = os.path.join(folder_path, item)
-        ву
+        
         if os.path.isfile(item_path):
             file_extension = item.split('.')[-1].upper()
             normalized_name = normalize(item.split('.')[0])
@@ -38,6 +55,10 @@ def process_folder(folder_path):
                 destination_folder = 'audio'
             elif file_extension in ARCHIVE_EXTENSIONS:
                 destination_folder = 'archives'
+                archive_folder = os.path.join(folder_path, 'archives', normalized_name)
+                os.makedirs(archive_folder, exist_ok=True)
+                extract_archive(item_path, archive_folder)
+                continue
             else:
                 destination_folder = 'unknown'
                 new_name = item  
@@ -57,3 +78,4 @@ if __name__ == "__main__":
     else:
         target_folder = sys.argv[1]
         process_folder(target_folder)
+
